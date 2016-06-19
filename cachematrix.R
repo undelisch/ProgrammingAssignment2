fd90a0f282278f1465c79ca5556d10145f3e6e0e
Undine Schellhorn <undineschellhorn@gmail.com>
2016-06-19 13:53:47 +0200

    Put some code into the template file. Constructing a function to calculate the invers of a 2by2 matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+## application of lexical scoping in returning the inverse of a 2x2 matrix
 
-## Write a short comment describing this function
+## create a list of functions so they may later be applied to a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      
+      # NULL the content of "inv"
+      inv <- NULL
+      
+      # assign to x the value fed to "set()" & NULL the content of "inv"
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      
+      # read the value of x into a function called "get()"
+      get <- function() x
+      
+      # define a function that assigns to "inv" the value of "solved"
+      setinv <- function(solved) inv <<- solved
+      
+      # read the value of m into a function called "getmean()"
+      getinv <- function() inv
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## call cache and solve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+      # read the value of "inv"
+      inv <- x$getinv()
+      
+      # check whether inverse "inv" already exists (i.e. is not NULL)
+      if(!is.null(inv)) {
+            message(".:. getting cached data .:.")
+            return(inv)
+      }
+      
+      # otherwise move on and store tha value of the original matrix in "data"
+      data <- x$get()
+      
+      # calculate the inverse of "data" 
+      inv <- solve(data, ...)
+      
+      # call the caching function to assign the result to "inv" & return it
+      x$setinv(inv)
+      inv
 }
+
+## How to use this function? Just try this & you'll see:
+
+## to test this function you may for example use the invertible
+## matrix a = matrix(c(1,1,-1,1),ncol=2, byrow = TRUE) - note that
+## more than a lot of matrices aren't even invertible... 
+
+# a <- matrix(c(1,1,-1,1),ncol=2, byrow = TRUE)
+
+## the inverse of a is: 
+# a_inv <- matrix(0.5*c(1,-1,1,1),ncol=2, byrow = TRUE)
+
+# b <- makeCacheMatrix(a)
+# cacheSolve(b) == a_inv
+
+## check output:
+## .:. getting cached data .:.
+## [,1] [,2]
+## [1,] TRUE TRUE
+## [2,] TRUE TRUE
+
+## check some more matrices if you like <3
